a74b112563ebc8c968e53c5a5746b13a1c82ae1f
Gaute Berge <gaute.berge@gmail.com>
2020-10-15 13:33:50 +0000

    start script

---
+++ start.sh
@@ -0,0 +1,17 @@
+
+git pull
+export ADDR="0.0.0.0"
+export PORT="9160"
+
+# Start frontend
+cd frontend
+elm-live src/Main.elm \
+	--no-reload \
+	--port=8000 \
+       	--host="0.0.0.0" \
+	--start-page=src/index.html \
+	-- --output=public/main.js &
+
+# Start backend
+cd ../backend
+stack run
